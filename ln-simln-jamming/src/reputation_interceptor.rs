a9ab1b4d5183f9db830ffa5dd4a76483fba73887
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2025-06-17 15:11:30 -0400

    ln-simln-jamming/refactor: make ForwardReporter async

--- ln-simln-jamming/src/reputation_interceptor.rs
+++ ln-simln-jamming/src/reputation_interceptor.rs
@@ -397,6 +397,7 @@ where
                         allocation_check,
                         htlc_add.htlc.clone(),
                     )
+                    .await
                     .map_err(|e| ReputationError::ErrUnrecoverable(e.to_string()))?;
             }
         }
